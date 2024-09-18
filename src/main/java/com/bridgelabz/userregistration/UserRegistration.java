eea16f5d04c64a5ba6fbb258ea611fc794258a1c
Kajalwankhede <kajuwankhede151@gmail.com>
2024-09-18 21:13:23 +0530

    UC1-As a User need to enter a valid first name.

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,7 +1,17 @@
 package com.bridgelabz.userregistration;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    public static boolean isValidFirstName(String firstName) {
+        String regex = "[A-Z][a-z]{3,}";
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(firstName).matches();
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Problem");
+        String testName1 = "Kajal";
+        System.out.println(testName1 + " is " + (isValidFirstName(testName1) ? "a valid" : "an invalid") + " first name.");
     }
 }
--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -1,4 +1,18 @@
 package com.bridgelabz.userregistration;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserRegistrationTest {
+
+    @Test
+    public void FirstNameCorrectTest() {
+        boolean result = UserRegistration.isValidFirstName("Kajal");
+        Assert.assertTrue(result);    }
+
+    @Test
+    public void FirstNameIncorrectTest() {
+        boolean result = UserRegistration.isValidFirstName("payal");
+        Assert.assertFalse(result);
+    }
 }
